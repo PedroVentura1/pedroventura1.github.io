f21f41d39640568c2c2b540563ebab033b5e2e04
PedroVentura1 <peyvent27@gmail.com>
2024-08-15 13:46:54 -0600

    Update script.js
    Adding the JS code

--- js/script.js
+++ js/script.js
@@ -1,168 +1,53 @@
-const addAttributes = (element, attrObj) => {
-      for (let attr in attrObj) {
-        if (attrObj.hasOwnProperty(attr))
-      element.setAttribute(attr,attrObj[attr])
-      }
-    };
+$(document).ready(function() {
+    // Initial setup: add 'activo' class to the first link and show the first article
+    $('ul.cases li a:first').addClass('activo');
+    $('.pages2 article').hide();
+    $('.pages2 article:first').show();
 
-    //Crear elementos con atributos e hijo
-    const createCustomElement = (element,attributes,children) => {
-      let customElement = document.createElement(element);
-      if (children !== undefined) children.forEach(el => {
-        if(el.nodeType) {
-          if (el.nodeType === 1 || el.nodeType === 11)
-      customElement.appendChild(el);
-      } else {
-        customElement.innerHTML += el;
-      }
-    });
-      addAttributes(customElement,attributes);
-      return customElement;
-    };
+    // Event handler for link clicks
+    $('ul.cases li a').on('click', function(event) {
+        event.preventDefault();
 
-    // Imprimir modal
-    const printModal = content => {
-      // crear contenedor interno
-      const modalContentEl = createCustomElement('div', {
-        id: 'ed-modal-content',
-        class: 'ed-modal-content'
-      }, [content]),
+        // Remove multiple classes from all links
+        $('ul.cases li a').removeClass('activo active_2 active_3 activo_green activo_red');
 
-    // crear contenedor principal
-      modalContainerEl = createCustomElement('div', {
-        id: 'ed-modal-container',
-        class: 'ed-modal-container'
-      }, [modalContentEl]);
+        // Get the href attribute of the clicked link
+        var activeTab = $(this).attr('href');
+        console.log(activeTab);
 
-      // Imprimir el modal
-      document.body.appendChild(modalContainerEl);
+        // Add different styles based on the href attribute
+        if (activeTab === '#page1') {
+            $(this).addClass('activo');
+        } else if (activeTab === '#page2') {
+            $(this).addClass('activo_red');
+        } else if (activeTab === '#page3') {
+            $(this).addClass('activo_green');
+        }
 
-      //  Remover el modal
-      const removeModal = () => document.body.removeChild(modalContainerEl);
-
-        modalContainerEl.addEventListener('click', e => {
-          if (e.target === modalContainerEl) removeModal();
-        })
-    }
-    
-
-    document.getElementById('verifyDetaill').addEventListener('click', () => {
-      var content = informacionVerify.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-   document.getElementById('verifyItemImg').addEventListener('click', () => {
-      var content = informacionVerify.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
+        // Hide all articles and show the corresponding article
+        $('.pages2 article').hide();
+        $(activeTab).show();
     });
 
+    // Smooth scroll for list items
+    document.querySelectorAll('.cases li a').forEach(item => {
+        item.addEventListener('click', event => {
+            const targetId = item.getAttribute('data-target');
+            if (targetId) {
+                const targetElement = document.getElementById(targetId);
+                if (targetElement) {
+                    const offsetTop = targetElement.offsetTop;
 
-    document.getElementById('sistemaActDetaill').addEventListener('click', () => {
-      var content = infoSisteActinver.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
+                    window.scroll({
+                        top: offsetTop,
+                        behavior: 'smooth'
+                    });
+                } else {
+                    console.error(`Element with ID ${targetId} not found.`);
+                }
+            } else {
+                console.error('data-target attribute not found on the clicked element.');
+            }
+        });
     });
-   document.getElementById('sisActItemImg').addEventListener('click', () => {
-      var content = infoSisteActinver.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-
-    document.getElementById('bbvaDetaill').addEventListener('click', () => {
-      var content = informacionBbva.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-   document.getElementById('bbvaItemImg').addEventListener('click', () => {
-      var content = informacionBbva.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-
-    document.getElementById('bursanetDetaill').addEventListener('click', () => {
-      var content = infoBursanet.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-   document.getElementById('bursanetItemImg').addEventListener('click', () => {
-      var content = infoBursanet.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-
-    document.getElementById('modeloDetaill').addEventListener('click', () => {
-      var content = infoModeloNow.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-   document.getElementById('modeloItemImg').addEventListener('click', () => {
-      var content = infoModeloNow.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-
-    document.getElementById('kanzleiDetaill').addEventListener('click', () => {
-      var content = infoKanzlei.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-   document.getElementById('kanzleiItemImg').addEventListener('click', () => {
-      var content = infoKanzlei.cloneNode(true)
-      content.style["display"] = "block";
-      printModal(content);
-    });
-
-  // inside slide detaill 
-    var i = 0,images = ["img/verify_max.png",
-                            "img/verify_home.png",
-                            "img/verify_instructions.png",
-                            "img/verify_Empty.png"];
-                            
-          function mySlide(param)
-          {
-              if(param === 'next')
-              {
-                  i++;
-                  if(i === images.length){ i = images.length - 1; }
-              }else{
-                  i--;
-                  if(i < 0){ i = 0; }
-              }              
-              document.getElementById('ed-modal-content').getElementsByClassName('slide')[0].src = images[i];  
-          }
-
-          var ib = 0,imagesb = ["img/actinverSistemaA.png",
-                                "img/chartActinver.png",
-                                "img/Sistema_A_wireframe.png"];                           
-          function mySlideb(param)
-          {
-              if(param === 'next')
-              {
-                  ib++;
-                  if(ib === imagesb.length){ ib = imagesb.length - 1; }
-              }else{
-                  ib--;
-                  if(ib < 0){ ib = 0; }
-              }
-              document.getElementById('ed-modal-content').getElementsByClassName('slide2')[0].src = imagesb[ib];
-          }
-
-          var ic = 0,imagesc = ["img/spendingBBVA.png",
-                                "img/spendingConfirm.png",
-                                "img/spendingSucces.png"];                            
-          function mySlidec(param)
-          {
-              if(param === 'next')
-              {
-                  ic++;
-                  if(ic === imagesc.length){ ic = imagesc.length - 1; }
-              }else{
-                  ic--;
-                  if(ic < 0){ ic = 0; }
-              }
-              document.getElementById('ed-modal-content').getElementsByClassName('slide3')[0].src = imagesc[ic];
-          }
-
-
-
+});
